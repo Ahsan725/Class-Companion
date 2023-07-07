851586e117e7d3b9184e15fb33cdda6f325c3ff0
Ahsan725 <125620778+Ahsan725@users.noreply.github.com>
2023-07-07 11:32:51 -0400

    Class Companion -initial commit

---
+++ SignUpPageScript.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  var form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    playSound();
+    window.location.href = 'nextpage.html';
+  });
+});
+
+function showAlert() {
+  alert("STAY TUNED FOR PERSONALIZED PLANNER FEATURE");
+}
